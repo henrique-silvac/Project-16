58c81c4885d091bf44db05a0db681ea4b9c8e7ef
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-08-10 16:42:43 -0300

    Wrap up

--- Project 16/Capital.swift
+++ Project 16/Capital.swift
@@ -5,18 +5,19 @@
 //  Created by Henrique Silva on 10/08/21.
 //
 
-import UIKit
 import MapKit
+import UIKit
 
 class Capital: NSObject, MKAnnotation {
-    
     var title: String?
     var coordinate: CLLocationCoordinate2D
     var info: String
+    var wikipediaUrl: String
     
-    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String, wikipediaUrl: String) {
         self.title = title
         self.coordinate = coordinate
         self.info = info
+        self.wikipediaUrl = wikipediaUrl
     }
 }
--- Project 16/ViewController.swift
+++ Project 16/ViewController.swift
@@ -5,54 +5,91 @@
 //  Created by Henrique Silva on 10/08/21.
 //
 
-import UIKit
 import MapKit
+import UIKit
 
 class ViewController: UIViewController, MKMapViewDelegate {
-    
+
     @IBOutlet var mapView: MKMapView!
+    
+    let mapTypes = ["hybrid": MKMapType.hybrid, "hybridFlyover": MKMapType.hybridFlyover, "mutedStandard": MKMapType.mutedStandard, "satellite": MKMapType.satellite, "satelliteFlyover": MKMapType.satelliteFlyover, "standard": MKMapType.standard]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
-        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
-        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light")
-        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
-        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
 
-        mapView.addAnnotations([london, oslo, paris, rome, washington])
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.", wikipediaUrl: "https://en.wikipedia.org/wiki/London")
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.", wikipediaUrl: "https://en.wikipedia.org/wiki/Oslo")
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8546, longitude: 2.3508), info: "Often called the City of Light.", wikipediaUrl: "https://en.wikipedia.org/wiki/Paris")
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.", wikipediaUrl: "https://en.wikipedia.org/wiki/Rome")
+        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.", wikipediaUrl: "https://en.wikipedia.org/wiki/Washington,_D.C.")
+        let saopaulo = Capital(title: "São Paulo", coordinate: CLLocationCoordinate2D(latitude: -23.3251, longitude: -46.3810 ), info: "Is the largest city in the country of Brazil.", wikipediaUrl: "https://en.wikipedia.org/wiki/S%C3%A3o_Paulo")
+
+        mapView.addAnnotations([london, oslo, paris, rome, washington, saopaulo])
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Map type", style: .plain, target: self, action: #selector(selectMapType))
     }
 
+    @objc func selectMapType() {
+        let ac = UIAlertController(title: "Map type", message: nil, preferredStyle: .actionSheet)
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        for mapType in Array(mapTypes.keys).sorted(by: <) {
+            ac.addAction(UIAlertAction(title: mapType, style: .default, handler: mapTypeSelected))
+        }
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        present(ac, animated: true)
+    }
+
+    func mapTypeSelected(action: UIAlertAction) {
+        guard let title = action.title else { return }
+
+        if let type = mapTypes[title] {
+            mapView.mapType = type
+        }
+    }
+    
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard annotation is Capital else { return nil }
-
+        
         let identifier = "Capital"
-
+        
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
-
+        
         if annotationView == nil {
             annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
             annotationView?.canShowCallout = true
-
+            
             let btn = UIButton(type: .detailDisclosure)
             annotationView?.rightCalloutAccessoryView = btn
-        } else {
-            annotationView?.annotation = annotation
         }
-
+        else {
+            annotationView?.annotation = annotation
+            
+            if let pinView = annotationView as? MKPinAnnotationView {
+                pinView.pinTintColor = UIColor(red: 0, green: 0.7, blue: 0, alpha: 1)
+            }
+        }
+        
         return annotationView
     }
-
+    
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         guard let capital = view.annotation as? Capital else { return }
-
         let placeName = capital.title
         let placeInfo = capital.info
-
+        
         let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Wikipedia", style: .default, handler: { [weak self] _ in
+            self?.openWikipedia(url: capital.wikipediaUrl)
+        }))
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
+    
+    func openWikipedia(url: String) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "WebViewController") as? WebViewController {
+            vc.website = url
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
-
